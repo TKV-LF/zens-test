01f27fbcb3f8022d5a630408287b7449b29ce7cb
ThuyNT <thuyngtrong@gmail.com>
2023-11-29 17:49:23 +0700

    Update front-end

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # zens-test
+# Joke Voting App
+
+This application allows users to view jokes and vote on them. It displays a single joke for the user to read, allows the user to like or dislike the joke, and records the vote in a database. When all available jokes have been voted on, it shows a message indicating the end of available jokes for the day.
+
+## Features
+
+- Display a single joke to the user.
+- Allow users to like or dislike the joke.
+- Record votes in a database.
+- Prevent users from seeing the same joke twice.
+- Use cookies to track user votes (even if the user clears their cookies).
+
+## Technology Stack
+
+- Frontend: HTML, CSS, JavaScript
+- Backend: PHP
+- Database: MySQL
+
+## Setup
+
+### Prerequisites
+
+- PHP
+- MySQL
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/TKV-LF/zens-test.git
--- php/src/front-end/index.php
+++ php/src/front-end/index.php
@@ -8,36 +8,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
   <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="style.css" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
 </head>
-<style>
-  .d-flex {
-    gap: 20px;
-  }
-
-  .footer .description {
-    color: #969696;
-  }
-</style>
 
 <body>
   <div class="container">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between">
       <div class="col-md-3 mb-2 mb-md-0">
-        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-          <img src="https://zen-s.com/wp-content/themes/zenS/Logo.svg" alt="">
+        <a href="#" class="d-inline-flex link-body-emphasis text-decoration-none">
+          <img src="img/logo.png" alt="">
         </a>
       </div>
       <div class="col-md-3 text-end">
         <div class="row">
           <div class="col-6">
             <div class="row float-right" style="color: #969696">Handicrapted by</div>
-            <div class="row float-right">Trong Thuy</div>
+            <div class="row float-right">Jim HLS</div>
           </div>
           <div class="col-6">
-            <img src="https://img.freepik.com/premium-vector/happy-flower-icon_76844-2641.jpg" width="50px"
-              height="50px" alt="">
+            <img src="img/avatar.png" width="50px" height="50px" alt="">
           </div>
         </div>
       </div>
@@ -45,22 +36,32 @@
   </div>
   <div class="sub-header" style='background-color: #00B65A'>
     <div class="container py-5">
-      <div class="h2 text-center text-white">A joke a day keeps the docter away</div>
-      <div class="p text-center text-white">If you joke wrong way, your teeth have to pay. (Serious)</div>
+      <div class="h2 text-center text-white mt-4 font-weight-bold">A joke a day keeps the docter away</div>
+      <div class="p text-center text-white mt-3 mb-5 font-weight-normal">If you joke wrong way, your teeth have to pay.
+        (Serious)</div>
     </div>
   </div>
   <div class="main border-bottom">
     <div class="container text-center py-5">
-      <p class="joke-text" id="jokeText" style='color: #646464'>Loading joke...</p>
-      <p class="joke-id d-none" id="jokeId">0</p>
-      <div class="button-container d-flex justify-content-center my-5 mx-5 px-5 py-4 border-top">
-        <button type='button' onclick="vote('like')" class='btn btn-primary w-25'>This is Funny!</button>
-        <button type='button' onclick="vote('dislike')" class='btn btn-success w-25'>This is not funny.</button>
+      <div class="content mb-5">
+        <p class="joke-text text-left" id="jokeText" style='color: #646464'>Loading joke...</p>
+        <p class="joke-id d-none" id="jokeId">0</p>
+      </div>
+      <div class="line text-center">
+        <hr class="w-75">
+      </div>
+      <div class="button-container m-0 m-md-4 p-0 p-md-4">
+        <div class="w-50 mx-auto row">
+          <button type='button' onclick="vote('like')" class='btn btn-primary col-12 col-md-5'>This is Funny!</button>
+          <div class="col-md-1"></div>
+          <button type='button' onclick="vote('dislike')" class='btn btn-success col-12 col-md-5 mt-md-0 mt-4'>This
+            is not funny.</button>
+        </div>
       </div>
     </div>
   </div>
   <div class="footer">
-    <div class="container text-center mt-5">
+    <div class="container text-center my-4">
       <div class="description">
         <div class="p">The website is created as part of Hlsolutions program. The materials contained on
           this
@@ -71,7 +72,7 @@
         <div class="p">acceps no liability for any loss or damage which may arise from reliance on the
           information contained this site</div>
       </div>
-      <div class='mt-3'>Copyright 2021 ThuyNT</div>
+      <div class='mt-3' style='color: #646464'>Copyright 2021 HLS</div>
     </div>
   </div>
 
